36a267f3e2d9688d0f32259831422d00a7b7eba1
medlamrani <45313009+medlamrani@users.noreply.github.com>
2020-08-16 16:46:08 +0100

    add entity

---
+++ src/lib/entity/Game.php
@@ -0,0 +1,87 @@
+<?php
+
+class Game 
+{
+    protected $id,
+              $name,
+              $platformId,
+              $editorId,
+              $releaseDate,
+              $ratingId;
+              
+    public function __construct( $values = [])
+    {    
+        if (!empty($values))
+        {
+            $this->hydrate($values);
+        }
+    }
+
+    public function hydrate($datas)
+    {
+        foreach ($datas as $attribut => $value)
+        {
+            $method = 'set'.ucfirst($attribut);
+
+            if (is_callable([$this, $method]))
+            {
+                $this->$method($value);
+            }
+        }
+    }  
+    
+    public function isNew()
+    {
+        return empty($this->id);
+    }
+
+    public function setId($id)
+    {
+        $this->id = (int) $id;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function setPlatformId($platformId)
+    {
+        $this->platformId = (int) $platformId;
+    }
+
+    public function setEditorId ($editorId)
+    {
+        $this->editorId = (int) $editorId;
+    }
+
+    public function setReleaseDate($releaseDate)
+    {
+        $this->releaseDate = $releaseDate;
+    }
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function platformId()
+    {
+        return $this->platformId;
+    }
+
+    public function editorId()
+    {
+        return $this->editorId;
+    }
+
+    public function releaseDate()
+    {
+        return $this->releaseDate;
+    }
+}
---
+++ src/lib/entity/News.php
@@ -0,0 +1,72 @@
+<?php
+
+class News
+{
+    protected $id,
+              $gameId,
+              $title,
+              $content,
+              $addDate,
+              $updateDate;
+
+    public function __construct( $values = [])
+    {    
+        if (!empty($values))
+        {
+            $this->hydrate($values);
+        }
+    }
+
+    public function hydrate($datas)
+    {
+        foreach ($datas as $attribut => $value)
+        {
+            $method = 'set'.ucfirst($attribut);
+
+            if (is_callable([$this, $method]))
+            {
+                $this->$method($value);
+            }
+        }
+    }      
+    
+    public function isNew()
+    {
+        return empty($this->id);
+    }
+
+    public function isValid()
+    {
+        return !(empty($this->title) || empty($this->content));
+    }
+
+    public function setId($id)
+    {
+        $this->id = (int) $id;
+    }
+
+    public function setGameId($gameId)
+    {
+        $this->gameId = (int) $gameId;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    public function setAddDate($addDate)
+    {
+        $this->addDate = $addDate;
+    }
+
+    public function setUpdateDate($updateDate)
+    {
+        $this->updateDate = $updateDate;
+    }
+}
