45a07d315b77a5b6a94194f5abbf0a79db174bb6
medlamrani <45313009+medlamrani@users.noreply.github.com>
2020-10-11 20:57:06 +0100

    rating

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # Projet "Culture Jeux Videos"
 
 * Site pour tout connaitre des jeux videos (date de creation, equipe, etc.. )
+
+
+
+# Prerequis pour lancer l'application :
+
+* Site dynamique (pour que votre pc puisse lire du php)
+* Editeur de text
+* Copier et coller le code dans l'editeur de text
+* Ouvrir Phpmyadmin et creer une base de donnee
+* Copier le code du fichier 00_create-table.sql ainsi que 01_insert-data.sql
+* Modifier le dossier DBConnect.php avec les informations de votre base de donnee
+* Lancer l'application
+
+# Environnement
+
+* PHP 
+* MVC
+* OOP
+* Bootstrap
+* MySQL
+
+# Liens 
+
+* Le lien de mon application : http://gameworld.web-devmed.fr/
+* Pour acceder a la partie administration : http://gameworld.web-devmed.fr/index.php?action=administration
+
+
+
--- src/public/js/rating.js
+++ src/public/js/rating.js
@@ -1,61 +1,12 @@
-class Rating{
-
-    constructor()
-    {
-        this.rate = document.getElementsByClassName('rateButton');
-        this.ratingForm = document.getElementById('ratingForm');
-        this.rating = document.getElementById('rating');
-        this.selectedStar = document.getElementsByClassName('star-selected');
-
-        this.ratingEvent();
+$( ".rateButton" ).click(function() {
+    if($(this).hasClass('star-grey')) {
+        $(this).removeClass('star-grey').addClass('star-highlight star-selected');
+        $(this).prevAll('.rateButton').removeClass('star-grey').addClass('star-highlight star-selected');
+        $(this).nextAll('.rateButton').removeClass('star-highlight star-selected').addClass('star-grey');
+    } else {
+        $(this).nextAll('.rateButton').removeClass('star-highlight star-selected').addClass('star-grey');
     }
-
-    ratingEvent()
-    {
-        
-        for(let elt of this.rate )  {
-            elt.addEventListener("click", this.starRate.bind(this , elt));
-        }; 
-        this.ratingForm.addEventListener("submit", this.formRate.bind(this));
-    }
-    
-    starRate(elt, event)
-    {
-        console.log(elt.classList);
-        if(elt.hasClass('star-grey'))
-        {
-            
-            elt.classList.remove('star-grey').classList.add('star-highlight star-selected');
-            elt.prevAll(elt).classList.remove('star-grey').classList.add('star-highlight star-selected');
-            elt.nextAll(elt).classList.remove('star-highlight star-selected').classList.add('star-grey');
-        }
-        else
-        {
-            elt.nextAll(elt).classList.remove('star-highlight star-selected').classList.add('star-grey');
-        }
-        this.rating.val(this.selectedStar.length);
-    }
-
-    formRate(e)
-    {
-        e.preventDefault();
-        let formData = this.ratingForm.serialize();
-
-        $.ajax({
-            type : 'POST',
-            dataType: 'json',
-            url : 'index.php',
-            data : formData,
-            success:function(response)
-            {
-                if(response.success == 1)
-                {
-                    this.ratingForm[0].reset();
-                    window.location.reload();
-                }
-            }
-        })
-    }
-}
+    $("#rating").val($('.star-selected').length);  //count the num of star selected
+});
 
   
